9569f2d8b1218e68f0c35cdaf89814de72875d4d
jiuduweiliangeer <64030931@qq.com>
2021-01-15 10:05:51 +0800

    Initial commit

---
+++ src/test/java/com/example/springboot03/SpringBoot03LoggingApplicationTests.java
@@ -0,0 +1,23 @@
+package com.example.springboot03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.slf4j.*;
+
+@SpringBootTest
+class SpringBoot03LoggingApplicationTests {
+    Logger logger= LoggerFactory.getLogger(getClass());
+    @Test
+    void contextLoads() {
+        //日志的级别
+        //由低到高 trace<debug<info<warn<error
+        //可以调整需要输出的日志级别：日志就只会在这个级别以及以后的高级别生效
+        logger.trace("这是trace日志...");
+        logger.debug("这是debug日志...");
+        //SpringBoot默认使用的是info级别的
+        logger.info("这是info日志...");
+        logger.warn("这是warn日志...");
+        logger.error("这是error日志...");
+    }
+
+}
